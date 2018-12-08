5dd7577c126b86fede153a32920b80429e608271
Igor Flyunt <igflyunt@gmail.com>
2018-12-08 18:46:14 +0200

    complete '5. Routing'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent }      from './heroes/heroes.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {HeroesComponent} from './heroes/heroes.component';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {HeroDetailsComponent} from "./hero-details/hero-details.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: 'detail/:id', component: HeroDetailsComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroDetailsComponent}
 ];
 
 
 @NgModule({
-  exports: [ RouterModule ],
+  exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
